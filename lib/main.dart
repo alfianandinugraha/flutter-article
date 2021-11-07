fae6d20f0ed0c0f1605d75069f3b5bcea6981094
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-07 17:08:59 +0700

    chore(app): init app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello !'),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage()
+      },
+    )
+  );
+}
