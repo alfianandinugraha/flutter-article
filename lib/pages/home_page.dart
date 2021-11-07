2d7c21317bd49e1125c7568ca397d66df094d282
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-07 21:51:42 +0700

    feat(widget): add SingleChildScrollView for HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const PageLayout(
-      child: ListPost(),
+      child: SingleChildScrollView(
+        child: ListPost(),
+      ),
     );
   }
 }
