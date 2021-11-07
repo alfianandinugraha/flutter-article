6cb78cea1361f89d5ed11afb822e0885294e40b8
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-07 21:16:42 +0700

    feat(widgets): add fetch api

--- lib/widgets/list_post.dart
+++ lib/widgets/list_post.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_article/models/post.dart';
+import 'package:http/http.dart' as http;
 
 class ListPost extends StatefulWidget {
   const ListPost({Key? key}) : super(key: key);
@@ -8,12 +12,33 @@ class ListPost extends StatefulWidget {
 }
 
 class _ListPostState extends State<ListPost> {
+  List<Post> posts = [];
+  bool isFetching = true;
+
+  void fetchPost() async {
+    Uri uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await http.get(uri);
+    var result = json.decode(response.body) as List<dynamic>;
+    List<Post> postsResult = result.map((e) {
+      return Post(id: e['id'], body: e['body'], userId: e['userId'], title: e['title']);
+    }).toList();
+
+    setState(() {
+      posts = postsResult;
+      isFetching = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPost();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [
-        Text("Hello !")
-      ],
+      children: const [Text("Hello !")],
     );
   }
 }
